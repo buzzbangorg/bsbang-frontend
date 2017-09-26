3576faeae0e16e02f941ef94b5bda720dc53d8fb
Justin Clark-Casey <justincc@justincc.org>
2017-09-26 17:53:46 +0100

    change bsbang-query.py to use the default _text_ catch all field

--- bsbang-query.py
+++ bsbang-query.py
@@ -4,7 +4,7 @@ import requests
 
 solrQueryPath = 'http://localhost:8983/solr/bsbang/select'
 
-params = {'q': 'name:b4401', 'defType': 'edismax'}
+params = {'q': 'b4401', 'defType': 'edismax'}
 
 r = requests.post(solrQueryPath, data=params)
 print(r.text)
