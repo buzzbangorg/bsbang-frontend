e2b6206e82395713da1b49947c88425db6a16f74
Justin Clark-Casey <justincc@justincc.org>
2017-09-27 15:15:24 +0100

    add csrf

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-from flask import Flask, render_template
-from wtforms import Form, StringField
+from flask import Flask, render_template, flash, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = SearchForm()
+    if form.validate():
+        flash('Query for [%s]' % form.q.data)
+        return redirect('/')
+
     return render_template('index.html', form=form)
 
 
-class SearchForm(Form):
+class SearchForm(FlaskForm):
     q = StringField('q', validators=[DataRequired()])
 
 
