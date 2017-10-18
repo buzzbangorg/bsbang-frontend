c21a330f575ddd726e3533317ed6eab65d642a92
Justin Clark-Casey <justinccdev@gmail.com>
2017-10-18 19:32:19 +0100

    update bsbang-query.py to accept an actual query

--- bsbang-query.py
+++ bsbang-query.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
+import argparse
 import requests
 
 solrQueryPath = 'http://localhost:8983/solr/bsbang/select'
 
-params = {'q': 'b4401', 'defType': 'edismax'}
+# MAIN
+parser = argparse.ArgumentParser('Run a test query against the Solr instance')
+parser.add_argument('query')
+args = parser.parse_args()
+
+params = {'q': args.query, 'defType': 'edismax'}
 
 r = requests.post(solrQueryPath, data=params)
 print(r.text)
